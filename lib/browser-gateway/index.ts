6fd3f4ac79a599e2cc1574986ef468df57e572d8
Juan Zambrano <jagzao@gmail.com>
2026-09-04 06:36:56 -0600

    feat: expose browser gateway scaffold

---
+++ lib/browser-gateway/index.ts
@@ -0,0 +1,5 @@
+export * from './domain/browser-engine.js';
+export * from './domain/browser-task.js';
+export * from './domain/browser-result.js';
+export * from './application/fallback-policy.js';
+export * from './application/browser-engine-router.js';
